e1903a011e06be973a2db2d4e46e7cc51f041909
tomada <tmasuyama1114@gmail.com>
2026-01-30 10:45:24 -0600

    refactor: remove search from docs sidebar and mobile menu

--- src/components/docs/DocsMobileMenu.tsx
+++ src/components/docs/DocsMobileMenu.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { MobileNavigation } from "@/components/MobileNavigation"
-import { Search } from "@/components/Search"
 import { shouldShowNavigation } from "@/lib/navigation-utils"
 import { usePathname } from "next/navigation"
 
@@ -16,7 +15,6 @@ export function DocsMobileMenu() {
   return (
     <div className="flex items-center gap-4 border-b border-gray-200 px-4 py-3 lg:hidden">
       <MobileNavigation />
-      <Search />
     </div>
   )
 }
--- src/components/docs/DocsSidebar.tsx
+++ src/components/docs/DocsSidebar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Navigation } from "@/components/Navigation"
-import { Search } from "@/components/Search"
 import { shouldShowNavigation } from "@/lib/navigation-utils"
 import { usePathname } from "next/navigation"
 
@@ -16,8 +15,7 @@ export function DocsSidebar() {
   return (
     <div className="hidden lg:relative lg:block lg:flex-none">
       <div className="sticky top-0 -ml-0.5 h-[calc(100vh-4.5rem)] w-64 overflow-x-hidden overflow-y-auto py-16 pr-8 pl-0.5 xl:w-72 xl:pr-16">
-        <Search />
-        <Navigation className="mt-6" />
+        <Navigation />
       </div>
     </div>
   )
