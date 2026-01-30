7146604731b283d6410baf13ac078afe597cc983
tomada <tmasuyama1114@gmail.com>
2026-01-30 10:45:25 -0600

    chore: remove search-related files and dependencies

--- src/markdoc/search.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@/markdoc/search.mjs" {
-  export interface Result {
-    url: string
-    title: string
-    pageTitle?: string
-    [key: string]: unknown
-  }
-  export function search(query: string, options?: { limit?: number }): Result[]
-}
