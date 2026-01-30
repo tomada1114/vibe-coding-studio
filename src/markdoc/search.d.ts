b8315d1afc8c27ed17e0c04559ec69dca19ec84c
tomada <tmasuyama1114@gmail.com>
2026-01-29 19:14:49 -0600

    feat: add markdoc configuration and type definitions

---
+++ src/markdoc/search.d.ts
@@ -0,0 +1,9 @@
+declare module '@/markdoc/search.mjs' {
+  export interface Result {
+    url: string
+    title: string
+    pageTitle?: string
+    [key: string]: unknown
+  }
+  export function search(query: string, options?: { limit?: number }): Result[]
+}
